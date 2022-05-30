5daa17ce8c46532b12d09c3db55ca7182754119e
limweb <lim.thongchai@gmail.com>
2022-05-30 17:44:07 +0700

    3 files modified, 1 deleted

--- vue/pages/Home.js
+++ vue/pages/Home.js
@@ -1,7 +1,12 @@
 export default {
     inheritAttrs: false,
     template: `
-    <div><h1>Test Home</h1> {{uuid}}</div>
+    <div class="bg-green-200 w-full h-full min-h-screen" >
+    <h1 class="bg-red-200 my-5 mx-1 ">Test Home 123</h1> 
+      <div class="w-full bg-blue-200 items-center align-center">
+        <span class="bg-yellow-300 w-full text-center">{{uuid}}</span>
+      </div>
+    </div>
     `,
     mixins: [],
     props:["value"],
